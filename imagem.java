23289e72472d88f8f967b9ddc1426ae477b3ab12
Elek Almeida <kaykealmeida2017@gmail.com>
2026-03-28 09:54:46 -0300

    Add files via upload

---
+++ imagem.java
@@ -0,0 +1,13 @@
+public class imagem {
+    public static void main(String[] args) {
+
+        System.out.println(
+            " +\"\"\"\"\"+\n" +
+            "[| o o |]\n" +
+            " |  ^  |\n" +
+            " | '-' |\n" +
+            " +-----+"
+        );
+
+    }
+}
